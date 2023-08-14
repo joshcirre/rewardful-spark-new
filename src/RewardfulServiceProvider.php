734cf99a62eb6d9d6d1f2406a84a96218862ab0b
Josh Cirre <joshcirre@gmail.com>
2023-08-14 12:02:01 -0700

    fix for new spark

--- src/RewardfulServiceProvider.php
+++ src/RewardfulServiceProvider.php
@@ -3,8 +3,8 @@
 namespace Rewardful\RewardfulSpark;
 
 use Illuminate\Support\Facades\Blade;
-use Laravel\Spark\Spark;
-use Laravel\Spark\Providers\AppServiceProvider as ServiceProvider;
+use Spark\Spark;
+use App\Providers\SparkServiceProvider as ServiceProvider;
 use Laravel\Spark\Contracts\Interactions\Settings\PaymentMethod\UpdatePaymentMethod;
 use Rewardful\RewardfulSpark\UpdateStripePaymentMethod;
 
@@ -15,20 +15,23 @@ class RewardfulServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        Spark::swap(UpdatePaymentMethod::class.'@handle', UpdateStripePaymentMethod::class.'@handle');
+        Spark::swap(UpdatePaymentMethod::class . '@handle', UpdateStripePaymentMethod::class . '@handle');
 
         $this->publishes([
-            __DIR__.'/config/rewardful.php' => config_path('rewardful.php'),
+            __DIR__ . '/config/rewardful.php' => config_path('rewardful.php'),
         ], 'rewardful-config');
 
         $this->publishes([
-            __DIR__.'/resources/js' => resource_path('js/rewardful'),
+            __DIR__ . '/resources/js' => resource_path('js/rewardful'),
         ], 'rewardful-vue');
 
         Blade::directive('rewardful_js', function () {
-            return "<script async src='https://r.wdfl.co/rw.js' data-rewardful='".config("rewardful.api_key")."'></script>";
+            $key = config("rewardful.api_key");
+            return "
+<script>(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');</script>
+<script async src='https://r.wdfl.co/rw.js' data-rewardful='" . $key . "'></script>";
         });
     }
 }
